201afcfcd0fcd0fee95afabb890f38aba4fca27a
edy alves <30.edyalves@gmail.com>
2025-07-17 18:31:23 -0300

    Adicionando label like e nope conforme swipe desliza para esquerda ou direita.

--- TinderApp/ViewModels/CardView.swift
+++ TinderApp/ViewModels/CardView.swift
@@ -13,6 +13,26 @@ class CardView: UIView {
     private let nameLabel = UILabel()
     private let bioLabel = UILabel()
     
+    //MARK: Label flutuante
+    
+    private let likeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "LIKE"
+        label.font = .boldSystemFont(ofSize: 32)
+        label.textColor = .green
+        label.alpha = 0
+        return label
+    }()
+    
+    private let nopeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "NOPE"
+        label.font = .boldSystemFont(ofSize: 32)
+        label.textColor = .red
+        label.alpha = 0
+        return label
+    }()
+    
     init(user: User){
         super.init(frame: .zero)
         setupLayout()
@@ -47,7 +67,7 @@ class CardView: UIView {
         bioLabel.textColor = .white
         bioLabel.numberOfLines = 0
         
-        [imageView, nameLabel, bioLabel].forEach {
+        [imageView, nameLabel, bioLabel, likeLabel, nopeLabel].forEach {
             addSubview($0)
             $0.translatesAutoresizingMaskIntoConstraints = false
         }
@@ -64,6 +84,12 @@ class CardView: UIView {
             bioLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
             bioLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -16),
             bioLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            
+            likeLabel.topAnchor.constraint(equalTo: topAnchor, constant: 32),
+            likeLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 32),
+            
+            nopeLabel.topAnchor.constraint(equalTo: topAnchor, constant: 32),
+            nopeLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -32)
         ])
     }
     
@@ -78,6 +104,9 @@ class CardView: UIView {
         let rotationAngle = rotationDegrees * .pi / 180
         let transform = CGAffineTransform(rotationAngle: rotationAngle).translatedBy(x: translation.x, y: translation.y)
         self.transform = transform
+        
+        likeLabel.alpha = translation.x > 0 ? min(1, abs(translation.x) / 100) : 0
+        nopeLabel.alpha = translation.x < 0 ? min(1, abs(translation.x) / 100) : 0
 
         if gesture.state == .ended {
             let shouldDismiss = abs(translation.x) > 120
@@ -92,6 +121,8 @@ class CardView: UIView {
             } else {
                 UIView.animate(withDuration: 0.3) {
                     self.transform = .identity
+                    self.likeLabel.alpha = 0
+                    self.nopeLabel.alpha = 0
                 }
             }
         }
