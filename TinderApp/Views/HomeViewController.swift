94d032a58216b4bb17a4a4c5b2252035b4ca4067
edy alves <30.edyalves@gmail.com>
2025-07-18 14:27:25 -0300

    Adicionando botões na homeView para like e dislike.

--- TinderApp/Views/HomeViewController.swift
+++ TinderApp/Views/HomeViewController.swift
@@ -15,10 +15,44 @@ class HomeViewController: UIViewController {
         User(id: 3, name: "Rafael", age: 30, bio: "Engenheiro de software e maratonista.", imageName: "rafael")
     ]
 
+    private let likeButton: UIButton = {
+        let btn = UIButton()
+        btn.setImage(UIImage(systemName: "hand.thumbsup.fill"), for: .normal)
+        btn.tintColor = .green.withAlphaComponent(0.6)
+        return btn
+    }()
+    
+    private let dislikeButton: UIButton = {
+        let btn = UIButton()
+        btn.setImage(UIImage(systemName: "hand.thumbsdown.fill"), for: .normal)
+        btn.tintColor = .red.withAlphaComponent(0.6)
+        return btn
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         layoutCards()
+        setupViews()
+    }
+    
+    private func setupViews() {
+        [likeButton, dislikeButton].forEach {
+            view.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        NSLayoutConstraint.activate([
+            dislikeButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10),
+            dislikeButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 60),
+            dislikeButton.widthAnchor.constraint(equalToConstant: 60),
+            dislikeButton.heightAnchor.constraint(equalToConstant: 60),
+
+            likeButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10),
+            likeButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -60),
+            likeButton.widthAnchor.constraint(equalToConstant: 60),
+            likeButton.heightAnchor.constraint(equalToConstant: 60),
+        ])
     }
     
     private func layoutCards() {
@@ -38,4 +72,6 @@ class HomeViewController: UIViewController {
             view.sendSubviewToBack(card)
         }
     }
-    }
+    
+    
+}
